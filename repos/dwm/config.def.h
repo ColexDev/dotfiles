38451f4e48e51fd9afa7ad75de25f3ee6410721e
ColexDev <cole@colex.dev>
2021-12-19 16:00:35 -0500

    Added gaps

--- repos/dwm/config.def.h
+++ repos/dwm/config.def.h
@@ -2,11 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=10" };
-static const char dmenufont[]       = "Source Code Pro:size=10";
+static const char *fonts[]          = {"Source Code Pro:size=11"};
+static const char dmenufont[]       = "Source Code Pro:size=11";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#ffffff";
 static const char col_gray3[]       = "#ffffff";
@@ -20,6 +21,10 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7"};
+
+static const char ptagf[] = "[%s %s]";	/* format of a tag label */
+static const char etagf[] = "[%s]";	/* format of an empty tag */
+static const int lcaselbl = 0;		/* 1 means make tag label lowercase */	
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -66,6 +71,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
+#include "movestack.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
@@ -91,6 +97,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_h,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_l, tagmon,         {.i = +1 } },
+    // not working???
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
     { MODKEY|ShiftMask,             XK_0,      quit,          {0} },
 	{ 0,                            XK_Print,  spawn,     SHCMD("flameshot gui") },
 	{ MODKEY|ShiftMask,             XK_e,      spawn,     SHCMD("slock") },
@@ -102,6 +111,9 @@ static Key keys[] = {
     { MODKEY|ControlMask,           XK_h,      spawn,    SHCMD("exec xdotool mousemove_relative -- -15 0") },
     { MODKEY|ControlMask,          XK_Return, spawn,    SHCMD("exec xdotool click 1") },
     { MODKEY|ShiftMask,          XK_slash, spawn,    SHCMD("exec pulseaudio-equalizer toggle") },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 
 
 	TAGKEYS(                        XK_1,                      0)
