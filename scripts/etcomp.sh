1239996097add3c6a407153ffcf5195d9aa6b34a
Cole <cole@colex.dev>
2022-03-06 20:30:57 -0500

    Added new script files

---
+++ scripts/etcomp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -r $HOME/ssd/calcurse $HOME/.local/share
+cp -r $HOME/ssd/timeblock $HOME/
---
+++ scripts/utssd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp -r $HOME/.local/share/calcurse $HOME/ssd/
+cp -r $HOME/timeblock $HOME/ssd/
